a04f4b58f8ae66a22845af143fcd911fd74732dd
AnasKhan20 <143949011+AnasKhan20@users.noreply.github.com>
2023-10-05 00:45:07 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,123 @@
+// Question 1
+
+// var currenttime = new Date()
+// alert(currenttime)
+
+
+// Question 2
+
+// var firstName="Anas"
+// var lastName="Khan"
+// var fullName=firstName + lastName
+
+// alert("Hello Mr"+ fullName)
+
+
+// Question 3
+
+// var num1= +prompt("enter first number")
+
+// var num2= +prompt("enter first number")
+
+// var sum = num1 + num2
+
+// console.log(sum)
+
+// Question 4
+
+// function calc(num1,opr,num2){
+// if(opr==="+"){
+//     return num1 + num2
+// }
+// else if(opr==="-"){
+//     return num1 - num2
+// }
+// else if(opr==="*"){
+//     return num1 * num2
+// }
+// else if(opr==="/"){
+//     return num1 / num2
+// }
+// }
+
+// var num1 = +prompt("Enter first number")
+// var opr = prompt("Enter operator")
+// var num2 = +prompt("Enter second number")
+
+// var result = calc(num1,opr,num2)
+
+// console.log(result)
+
+
+// Question 7
+
+// function num(a,b){
+//     for(var i=a;i<=b;i++){
+//         console.log(res)
+//     }
+// }
+
+// var a = +prompt("enter first number")
+// var b = +prompt("enter end number")
+
+// var res = num(a,b)
+
+
+// Question 8
+
+// outer function(hyp){
+//     inner function(squ){
+
+//     }
+
+// }
+
+// Qusstion 9
+
+// function area(a,b){
+// console.log(mult)
+// }
+// var a = +prompt("enter a width")
+// var b = +prompt("enter a height")
+// var mult = a * b
+// var mult = area(a,b)
+
+// Question 10
+
+// var text = prompt("enter a word")
+// var check= "";
+
+// for(var i=text.length-1;i>=0;i--){
+    
+// }
+
+// if(text===check){
+//     console.log("its a palindrome")
+// }
+// else{
+//     console.log("it is not a palindrome")
+// }
+
+// Question 11
+
+// function text(a){
+// var b = a.toUpperCase()
+// console.log(b)
+// }
+// var a = "Web Development Tutotial"
+
+// text(a)
+
+
+// Question 12
+
+// function text(arr){
+//     var arr = ["Web Development Tutotial"]
+//     console.log(arr[4,15])
+    
+// }
+// text(arr)
+
+
+// Question 14
+
